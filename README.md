9b2a3c5c7633e797c85b668b4754e329b30a841d
GabriellGimenes <gabrielgimenes157@gmail.com>
2023-10-23 09:58:07 -0300

    Final script limst-to-pims 23/10/2023

---
+++ README.md
@@ -0,0 +1,105 @@
+<html>
+<head>
+    <style>
+    .Title{
+        color: white;
+        border: 2px solid black;
+        margin: 20px;
+        padding: 10px;
+    }
+    .Systems{
+        color: white;
+        border: 2px solid black;
+        margin: 20px;
+        padding: 10px;
+    }  
+    .Work{
+        color: white;
+        border: 2px solid black;
+        margin: 20px;
+        padding: 10px;
+    }
+    .Funcionality{
+        color: white;
+        border: 2px solid black;
+        margin: 20px;
+        padding: 10px;   
+    }
+    .Executaveis{
+        color: white;
+        border: 2px solid black;
+        margin: 20px;
+        padding: 10px; 
+    }
+    .About{
+        color: white;
+        border: 2px solid black;
+        margin: 20px;
+        padding: 10px;
+    }
+    </style>
+</head>
+<body>
+
+<div class="Title">
+<h1 align="center"> Lims to Pims </h1>
+</div>
+
+<div class="Systems">
+<h2>📌 Principais Sistemas </h2>
+
+- MyLims
+    - Um LIMS é uma plataforma de software projetada para auxiliar laboratórios na organização, armazenamento, rastreamento e gerenciamento de amostras, dados e informações relacionadas às atividades laboratoriais.
+    
+- Pims
+    - Plant Information Management System é um software robusto e versátil que permite a gestão completa das informações da planta. Você pode conhecer o sistema PIMS como “Sistema de Gerenciamento de Informação de Planta”.
+
+ </div>
+
+<div class="Work">
+<h2 align="left">📌 Qual foi a solicitação? </h2>
+
+- A implementação foi desenvolvida para atender uma solicitação da área de produção, onde o solicitante necessitava de obter informações da aplicação MyLims, para realizar esse envio a outra aplicação denominada Pims.
+
+- Foi iniciado o estudo para entender quais seriam as melhores maneiras de realizar esta "integração". Ao decorrer do tempo, vimos que o MyLims já possuia essa integração pronta, porém a mesma não está contida em nossa licensa.
+
+</div>
+
+
+
+
+
+<div class="Funcionality">
+<H1 align="center">⚙️ Funcionalidade do script </H1>
+
+- `Funcionalidade 1`: Realiza a requisição da API fornecida pelo MyLims.
+- `Funcionalidade 2`: Pega e processa os dados recebidos da requisição.
+- `Funcionalidade 3`: Após realizar a requisição e processar os dados recebidos, é feito o envio dos dados para o Pims. 
+
+</br>
+
+<H1 align="center">📌 Sobre o script </H1>
+
+- `Sobre 1`: Foi desenvolvido em PowerShell, devido o SDK para desenvovilmento com o Pims.
+- `Sobre 2`: Utiliza bibliotecas do PySystem-Osisoft.
+- `Sobre 3`: Após finalizar a implementação, foi criado uma rotina em uma VM para executar a cada 01:00 hora. 
+
+</div>
+
+<div class="Executaveis">
+<H1 align="center">📌 Softwares a necessários </H1>
+
+- Acesse o link para entender melhor os sóftwares necessários para executar o script.
+- Link: https://docs.google.com/document/d/1VmP8dlpOPzZI_FNrm_Xu02gmpzhqh6qp/edit?usp=share_link&ouid=105886712674428510556&rtpof=true&sd=true
+
+</div>
+
+<div class="About">
+<H1 align="center">📌 Sobre </H1>
+
+- Para mais detalhes entre em contato pelo email gimenes.alencar2002@gmail.com.
+
+</body>
+</html>
+
+
---
+++ lims-to-pims.ps1
@@ -0,0 +1,140 @@
+﻿$inPIServer = "Your PI-Server here"
+$User = "User here"
+$Pword = ConvertTo-SecureString -String "Pword here" -AsPlainText -Force
+$Credential= New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $PWord
+$conn = Connect-PIDataArchive - PIDataArchiveMachineName $inPIServer
+$TimeStamp = Get-Date
+
+Import-Module Osisoft.PowerShell
+
+function GetAndProcessData($url, $token){
+    
+    $headers = @{
+        'x-access-key' = $token
+        'Accept' = 'application/json'
+    }
+
+    $response = Invoke-RestMethod -Uri $url -Headers $headers -Method Get
+    $results = $response.Result
+                           
+    foreach ($attribute in $results) {
+        $value = $attribute.DisplayValue
+        $data = $attribute.Sample.TakenDateTime
+        $date = Get-Date $data -Format "dd/MM/yyyy HH:mm:ss"
+        $Id = $attribute.Info.Id
+        $Identify = $attribute.Info.Identification
+        $valor = $associations[$globalId][$specifyId] 
+        $tagnamelist = @(valor)
+        $tags = $tagnamelist | % {Get-Point - Connection $conn -Name $_ -AllAtributes}
+        $DatePIMS = Get-PIValue - PIPoint $tags -Time $TimeStamp | % {$_.TimeStamp.tolocaltime()}
+        $DatePIMSF = Get-Date $DatePIMS -Format "dd/MM/yyyy HH:mm:ss"
+
+        if($date -gt $DatePIMSF){
+            ADD-PIValue - PointName $valor -Time $date -value -Connection $conn
+        }
+    }
+    $results | ConvertTo-Json | Set-Content -Path 'Your path here, to see a resul of json'
+}
+
+$associations = @{
+    10047 = @{
+        1062 = "myLIMS-P2O5-DMA"
+        1061 = "myLIMS-P2O5-DMB"
+    }
+    10016 = @{
+        1057 = "myLIMS-P2O5-DMA"
+        1106 = "myLIMS-P2O5-DMB"
+        1062 = "myLIMS-P2O5-PFGF"
+        1061 = "myLIMS-P2O5-PFF"
+        1115 = "myLIMS-P2O5-RRA"
+        1116 = "myLIMS-P2O5-RRB"
+    }
+    10017 = @{
+        1057 = "myLIMS-Fe2O3-DMA"
+        1106 = "myLIMS-Fe2O3-DMB"
+        1062 = "myLIMS-Fe2O3-PFGF"
+        1061 = "myLIMS-Fe2O3-PFF"
+        1115 = "myLIMS-Fe2O3-RRA"
+        1116 = "myLIMS-Fe2O3-RRB"
+    }
+    10018 = @{
+        1057 = "myLIMS-MgO-DMA"
+        1106 = "myLIMS-MgO-DMB"
+        1062 = "myLIMS-MgO-PFGF"
+        1061 = "myLIMS-MgO-PFF"
+        1115 = "myLIMS-MgO-RRA"
+        1116 = "myLIMS-MgO-RRB"
+    }
+    10019 = @{
+        1057 = "myLIMS-CaO-DMA"
+        1106 = "myLIMS-CaO-DMB"
+        1062 = "myLIMS-CaO-PFGF"
+        1061 = "myLIMS-CaO-PFF"
+        1115 = "myLIMS-CaO-RRA"
+        1116 = "myLIMS-CaO-RRB"
+    }
+    10020 = @{
+        1057 = "myLIMS-Al2O3-DMA"
+        1106 = "myLIMS-Al2O3-DMB"
+        1062 = "myLIMS-Al2O3-PFGF"
+        1061 = "myLIMS-Al2O3-PFF"
+        1115 = "myLIMS-Al2O3-RRA"
+        1116 = "myLIMS-Al2O3-RRB"
+    }
+    10021 = @{
+        1057 = "myLIMS-SiO-DMA"
+        1106 = "myLIMS-SiO-DMB"
+        1062 = "myLIMS-SiO-PFGF"
+        1061 = "myLIMS-SiO-PFF"
+        1115 = "myLIMS-SiO-RRA"
+        1116 = "myLIMS-SiO-RRB"
+    }
+    10022 = @{
+        1057 = "myLIMS-TiO-DMA"
+        1106 = "myLIMS-TiO-DMB"
+        1062 = "myLIMS-TiO-PFGF"
+        1061 = "myLIMS-TiO-PFF"
+        1115 = "myLIMS-TiO-RRA"
+        1116 = "myLIMS-TiO-RRB"
+    }
+    10023 = @{
+        1057 = "myLIMS-MnO-DMA"
+        1106 = "myLIMS-MnO-DMB"
+        1062 = "myLIMS-MnO-PFGF"
+        1061 = "myLIMS-MnO-PFF"
+        1115 = "myLIMS-MnO-RRA"
+        1116 = "myLIMS-MnO-RRB"
+    }
+    10024 = @{
+        1057 = "myLIMS-K2O-DMA"
+        1106 = "myLIMS-K2O-DMB"
+        1062 = "myLIMS-K2O-PFGF"
+        1061 = "myLIMS-K2O-PFF"
+        1115 = "myLIMS-K2O-RRA"
+        1116 = "myLIMS-K2O-RRB"
+    }
+}
+
+$apiUrl = "Your Lims apiUrl here"
+$token = "Your token here"
+
+$dtAtual = Get-Date       
+
+$dtIni = $dtAtual.AddDays(-20).ToUniversalTime()                          
+$dtIniFormated = $dtIni.ToString("yyyy-MM-ddTHH:mm:ssZ")            
+$dtFin = $dtAtual.ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ssZ")
+
+$results = $null
+
+$orderby = "orderby=Sample/TakenDateTime desc &$"
+$top = "top=1"
+
+foreach ($globalId in $associations.Keys) {
+    $specifyIds = $associations[$globalId]
+
+    foreach ($specifyId in $specifyIds.Keys) {
+        $filter = "filter=(Sample/Published eq true) and (Sample/Active eq true) and (Sample/Reviewed eq false) and (Sample/SampleReason/Id eq 1) and (Sample/SampleType/Id eq $specifyId) and (Info/Id eq $globalId) and (Sample/TakenDateTime ge datetime'$dtIniFormated' and Sample/TakenDateTime le datetime'$dtFin') and (ValueFloat ne null) &$"
+        $url = "$apiUrl$filter$orderby$top"
+        GetAndProcessData $url $token
+    }
+}
